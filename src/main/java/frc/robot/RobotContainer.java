5c59b8a8790f92221caddc963ac16ce8b8f1a7de
gmnielsen <nielsen@cheverus.org>
2021-04-02 00:46:29 -0400

    comments completed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,30 +11,75 @@ import frc.robot.subsystems.RomiDrivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * This is a definition of what a RobotContainer should be
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  /**
+   * first we declare our ingredients
+   */
   private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
+  /**
+   * private - this object can only be seen in this file
+   * 
+   * final - this object's name will never point to something else
+   * 
+   * RomiDrivetrain is the name of a subsystem - you can see it in the file list.
+   * But it is just a definition, not an object. When we make an "instance" of
+   * RomiDrivetrain, we need to "declare" what it is (in this case the object is a
+   * RomiDrivetrain) and give the instance a name, such as "m_romiDrivetrain".
+   * 
+   * new - we are making a new instance
+   * 
+   * RomiDriveTrain() - notice the parantheses!! - is the first method in the
+   * RomiDrivetrain subsystem and containes the instructions for making a
+   * RomiDrivetrain
+   */
+
+  /**
+   * CONVENTIONS for making names
+   * 
+   * m_ means my, mine, this is my instance
+   * 
+   * CamelCase (the first letter of each word, including the first word, is
+   * uppercase). We use this for the names of definitions, also called "types".
+   * Most things in CamelCase come with the program.
+   * 
+   * lowerCamelCase (not an uppercase Lower, but a lowercase lower). We use this
+   * for the names of instances of objects. Most of the things you'll make should
+   * follow the convention: m_lowerCamelCase
+   */
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * I prefer this example:
+   * 
+   * private final ChocolateChipCookie m_chocolateChipCookie = new
+   * ChocolateChipCookie();
+   * 
+   * you can eat m_chocolateChipCookie after following the ChocolateChipCookie()
+   * recipe that is found on the ChocolateChipCookie page.
+   */
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   * This container is also the recipe for making a m_robotContainer. (That
+   * happens in Robot.java)
+   * 
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
