da590c11eb62014845465b80a308bc5499b4417c
gmnielsen <nielsen@cheverus.org>
2021-04-02 13:50:49 -0400

    it drives

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,93 +1,89 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/**
+ * LESSON 2 - let's make it driveable!
+ * 
+ * We're also adding two files, a command called ArcadeDrive.java and a subsystem called RomiInputs.java.
+ * 
+ * (and remove lots of the unnecessary comments from WPI and from me)
+ */
 
+// libraries that we need
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.RomiDrivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-/**
- * This is a definition of what a RobotContainer should be
- */
+// ROBOTCONTAINER begins here
 public class RobotContainer {
   /**
    * first we declare our ingredients
    */
   private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
-  /**
-   * private - this object can only be seen in this file
-   * 
-   * final - this object's name will never point to something else
-   * 
-   * RomiDrivetrain is the name of a subsystem - you can see it in the file list.
-   * But it is just a definition, not an object. When we make an "instance" of
-   * RomiDrivetrain, we need to "declare" what it is (in this case the object is a
-   * RomiDrivetrain) and give the instance a name, such as "m_romiDrivetrain".
-   * 
-   * new - we are making a new instance
-   * 
-   * RomiDriveTrain() - notice the parantheses!! - is the first method in the
-   * RomiDrivetrain subsystem and containes the instructions for making a
-   * RomiDrivetrain
-   */
-
-  /**
-   * CONVENTIONS for making names
-   * 
-   * m_ means my, mine, this is my instance
-   * 
-   * CamelCase (the first letter of each word, including the first word, is
-   * uppercase). We use this for the names of definitions, also called "types".
-   * Most things in CamelCase come with the program.
-   * 
-   * lowerCamelCase (not an uppercase Lower, but a lowercase lower). We use this
-   * for the names of instances of objects. Most of the things you'll make should
-   * follow the convention: m_lowerCamelCase
-   */
-
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
 
   /**
-   * I prefer this example:
-   * 
-   * private final ChocolateChipCookie m_chocolateChipCookie = new
-   * ChocolateChipCookie();
-   * 
-   * you can eat m_chocolateChipCookie after following the ChocolateChipCookie()
-   * recipe that is found on the ChocolateChipCookie page.
+   * NEW INGREDIENTS: the xBox controller and a selection box
    */
+  private final XboxController m_xBox = new XboxController(0);
+  // the 0 at the end is the USB port that the xBox controller gets plugged into
+  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
   /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   * This container is also the recipe for making a m_robotContainer. (That
-   * happens in Robot.java)
-   * 
+   * We'll add commands to m_chooser, and then we can have those commands as
+   * choices for our autonomous routine. But that's for the next lesson.
    */
+
+  // ROBOTCONTAINER instructions
   public RobotContainer() {
-    // Configure the button bindings
+    /*
+     * lets make the default style of driving to arcade style. Runcommand() is a
+     * convenience command. Convenient doesn't mean easy to understand with things
+     * such as "() ->". I'll go into this in the next lesson.
+     */
+    m_romiDrivetrain.setDefaultCommand(
+        new RunCommand(() -> m_romiDrivetrain.arcadeDrive(-m_xBox.getRawAxis(1), m_xBox.getRawAxis(4))));
+    // configureButtonBindings() is defined below
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by instantiating a {@link GenericHID} or one of its subclasses
-   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
-   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
+  // ROBOTCONTAINER methods
+
+  // Configure the button bindings
   private void configureButtonBindings() {
+    /**
+     * Make a button that reads the light sensor. This doesn't do anything until we
+     * put the method into the InstandCommand()
+     * 
+     * By typing "m_x" you should only get "m_xBox" as a choice. By typing a "."
+     * after XboxController, you'll see "Button" as a choice. Another "." and you'll
+     * see all the buttons. And we need the "value" of the button. JoystickButton
+     * has lots of methods associated with it, and you get those by typing a ".".
+     * We'll use "whenPressed()".
+     * 
+     * I'll describe what an InstantCommand() is next lesson.
+     */
+    new JoystickButton(m_xBox, XboxController.Button.kB.value).whenPressed(new InstantCommand());
   }
 
   /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
+   * By using a SendableChooser<Command> structure, we can pick from a list on the
+   * computer to get the autonomous command we want. By typing "m_c" you should
+   * only get "m_chooser" as a choice. By typing a "." after m_chooser you will
+   * get a list of possible commands, including "getSelected()".
+   * 
+   * This is "public" because it is used in Robot.java during the autonomous
+   * period of the game. If it was "private", Robot.java wouldn't be able to use
+   * it.
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return m_chooser.getSelected();
   }
+
 }
