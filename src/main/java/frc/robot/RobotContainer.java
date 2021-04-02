81b12b08179b5c0e0683193cdede984314225337
gmnielsen <nielsen@cheverus.org>
2021-04-02 14:06:38 -0400

    lesson 1 complete

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,3 +1,7 @@
+/**
+ * LESSON 1 - What does all this stuff mean? What is it used for?
+ */
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
