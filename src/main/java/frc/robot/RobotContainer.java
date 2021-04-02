892aa54999ac4bcbe1437f9a447cd4328859c771
gmnielsen <nielsen@cheverus.org>
2021-04-02 16:11:03 -0400

    lesson 3 is completed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,27 @@
 /**
- * LESSON 2 - let's make it driveable!
+ * LESSON 3 - let's make it driveable!
  * 
- * We're also adding two files, a command called ArcadeDrive.java and a subsystem called RomiInputs.java.
+ * We're also adding a subsystem file, called RomiInputs.java, which will handle the input from the light sensor.
  * 
- * (and remove lots of the unnecessary comments from WPI and from me)
+ * (and we'll clean up some unused parts of the code.)
  */
 
 // libraries that we need
 package frc.robot;
+
+/**
+ * notice that the next line is underlined. 
+ * We never use this. 
+ * You can either put two slashes in front of it or you can delete it. 
+ * I'll delete it in the next lesson.
+ */
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.RomiDrivetrain;
+import frc.robot.subsystems.RomiInputs;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -25,29 +33,16 @@ public class RobotContainer {
    * first we declare our ingredients
    */
   private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
+  // we don't use m_autocommand - delete or comment it.
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
-
-  /**
-   * NEW INGREDIENTS: the xBox controller and a selection box
-   */
   private final XboxController m_xBox = new XboxController(0);
   // the 0 at the end is the USB port that the xBox controller gets plugged into
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
-
-  /**
-   * We'll add commands to m_chooser, and then we can have those commands as
-   * choices for our autonomous routine. But that's for the next lesson.
-   */
+  private final RomiInputs m_romiInput = new RomiInputs();
 
   // ROBOTCONTAINER instructions
   public RobotContainer() {
-    /*
-     * lets make the default style of driving to arcade style. There is already a
-     * command for that in RomiDrivetrain.
-     * 
-     * Runcommand() is a convenience command. Convenient doesn't mean easy to
-     * understand with things such as "() ->". I'll go into this in the next lesson.
-     */
+    // default driving will be arcade
     m_romiDrivetrain.setDefaultCommand(new RunCommand(
         () -> m_romiDrivetrain.arcadeDrive(-m_xBox.getRawAxis(1), m_xBox.getRawAxis(4)), m_romiDrivetrain));
     // configureButtonBindings() is defined below
@@ -59,18 +54,16 @@ public class RobotContainer {
   // Configure the button bindings
   private void configureButtonBindings() {
     /**
-     * Make a button that reads the light sensor. This doesn't do anything until we
-     * put the method into the InstandCommand()
+     * Right click (or two finger click) on subsystems and at the very bottom select
+     * "Create a new class/command". Select "Subsystem (new)" and call the subsystem
+     * RomiInputs. Follow the instructions in RomiInputs and then come back here to
+     * put in the InstantCommand.
      * 
-     * By typing "m_x" you should only get "m_xBox" as a choice. By typing a "."
-     * after XboxController, you'll see "Button" as a choice. Another "." and you'll
-     * see all the buttons. And we need the "value" of the button. JoystickButton
-     * has lots of methods associated with it, and you get those by typing a ".".
-     * We'll use "whenPressed()".
-     * 
-     * I'll describe what an InstantCommand() is next lesson.
+     * The InstantCommand will run the printSensor method from our instance of
+     * RomiInputs, called m_romiInput
      */
-    new JoystickButton(m_xBox, XboxController.Button.kB.value).whenPressed(new InstantCommand());
+    new JoystickButton(m_xBox, XboxController.Button.kB.value)
+        .whenPressed(new InstantCommand(m_romiInput::printSensor, m_romiInput));
   }
 
   /**
