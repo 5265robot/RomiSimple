73843b52cf7eeef682596ed0c1c7336445a05160
gmnielsen <nielsen@cheverus.org>
2021-04-02 20:18:51 -0400

    prepping for final day

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,25 +1,8 @@
-/**
- * LESSON 3 - let's make it driveable!
- * 
- * We're also adding a subsystem file, called RomiInputs.java, which will handle the input from the light sensor.
- * 
- * (and we'll clean up some unused parts of the code.)
- */
-
 // libraries that we need
 package frc.robot;
 
-/**
- * notice that the next line is underlined. 
- * We never use this. 
- * You can either put two slashes in front of it or you can delete it. 
- * I'll delete it in the next lesson.
- */
-
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.RomiDrivetrain;
 import frc.robot.subsystems.RomiInputs;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -29,14 +12,8 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 // ROBOTCONTAINER begins here
 public class RobotContainer {
-  /**
-   * first we declare our ingredients
-   */
   private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
-  // we don't use m_autocommand - delete or comment it.
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
-  private final XboxController m_xBox = new XboxController(0);
-  // the 0 at the end is the USB port that the xBox controller gets plugged into
+  private final XboxController m_xBox = new XboxController(Constants.IO.Controller);
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
   private final RomiInputs m_romiInput = new RomiInputs();
 
@@ -53,29 +30,11 @@ public class RobotContainer {
 
   // Configure the button bindings
   private void configureButtonBindings() {
-    /**
-     * Right click (or two finger click) on subsystems and at the very bottom select
-     * "Create a new class/command". Select "Subsystem (new)" and call the subsystem
-     * RomiInputs. Follow the instructions in RomiInputs and then come back here to
-     * put in the InstantCommand.
-     * 
-     * The InstantCommand will run the printSensor method from our instance of
-     * RomiInputs, called m_romiInput
-     */
+    // testing the light sensor
     new JoystickButton(m_xBox, XboxController.Button.kB.value)
         .whenPressed(new InstantCommand(m_romiInput::printSensor, m_romiInput));
   }
 
-  /**
-   * By using a SendableChooser<Command> structure, we can pick from a list on the
-   * computer to get the autonomous command we want. By typing "m_c" you should
-   * only get "m_chooser" as a choice. By typing a "." after m_chooser you will
-   * get a list of possible commands, including "getSelected()".
-   * 
-   * This is "public" because it is used in Robot.java during the autonomous
-   * period of the game. If it was "private", Robot.java wouldn't be able to use
-   * it.
-   */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return m_chooser.getSelected();
--- src/main/java/frc/robot/subsystems/RomiInputs.java
+++ src/main/java/frc/robot/subsystems/RomiInputs.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 // libraries that we need.
 package frc.robot.subsystems;
 
@@ -11,39 +7,23 @@ import frc.robot.Constants;
 
 // ROMIINPUTS begins here
 public class RomiInputs extends SubsystemBase {
-  // create a variable for the light sensor
+  // light sensor
   private final AnalogPotentiometer m_lightSensor;
 
   // ROMIINPUTS instructions
   public RomiInputs() {
-    /**
-     * Make the light sensor. Notice that "Constants" has a squiggly red line
-     * underneath it for you. That's because it needs to be added (imported) to the
-     * libraries up above. Hover your mouse over it and you will see "Quick Fix".
-     * Select the first option. Go to "Constants" so we can add our numbers.
-     * 
-     * 
-     */
+    // create light sensor instance
     m_lightSensor = new AnalogPotentiometer(Constants.AnalogInputs.lightSensor);
   }
 
   // ROMIINPUTS methods
 
-  /**
-   * "double" is for a decimal number with double precision. We return that value
-   * with this command.
-   */
+  // get the value for the light sensor
   public double getLightSensor() {
     return m_lightSensor.get();
   }
 
-  /**
-   * "System.out.println" will write whatever we give it to the terminal on our
-   * computer. In this case we will be able to see the output of the light sensor
-   * to confirm what we are doing.
-   * 
-   * "void" means we don't return anything.
-   */
+  // put the value in the terminal
   public void printSensor(){
     System.out.println(getLightSensor());
   }
