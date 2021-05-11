a5c9961d4936b7e7d1f2bce00944d2537387ef75
nelsona92 <nelson.addison@cheverus.org>
2021-05-11 15:26:56 -0400

    many points

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,12 +18,15 @@ public final class Constants {
     public final static class path{
         public static class A {            
             static final Pose2d startPose = new Pose2d(0.0,0.0,new Rotation2d(Units.degreesToRadians(0)));
-            static final Pose2d endPose = new Pose2d(0.17,0.17,new Rotation2d(Units.degreesToRadians(0)));
+            static final Pose2d endPose = new Pose2d(0.17,0.19,new Rotation2d(Units.degreesToRadians(0)));
             static List<Translation2d> points = List.of(
                 //new Translation2d(0.2,0.0),
                 //new Translation2d(0.2,0.2)
-                new Translation2d(0.2, 0.0)
-                //new Translation2d(0.0,0.23)
+                new Translation2d(0.05, 0.01),
+                new Translation2d(0.1, 0.03),
+                new Translation2d(0.13, 0.05),
+                new Translation2d(0.16, 0.1),
+                new Translation2d(0.17, 0.14)
                 );
         }
         public static class B{            
